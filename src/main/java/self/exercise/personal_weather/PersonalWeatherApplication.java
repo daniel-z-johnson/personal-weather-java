f1f99d550c04cd039fe4269eb7eebef749e92d18
daniel-z-johnson <danzaljohnson@gmail.com>
2025-09-04 23:02:12 -0500

    Initial commit

---
+++ src/main/java/self/exercise/personal_weather/PersonalWeatherApplication.java
@@ -0,0 +1,13 @@
+package self.exercise.personal_weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/self/exercise/personal_weather/PersonalWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package self.exercise.personal_weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
